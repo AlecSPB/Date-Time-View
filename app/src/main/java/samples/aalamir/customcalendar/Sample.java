49d6e5e6ab2a1ad5c2a69e74f9ac0a4b64a3de8c
daffodil <shantanu.jain@daffodilsw.com>
2017-04-07 14:49:52 +0530

    Second Commit

---
+++ app/src/main/java/samples/aalamir/customcalendar/Sample.java
@@ -0,0 +1,13 @@
+package samples.aalamir.customcalendar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Sample extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
